9d74f9497bc42c62c987b078c8b439d55e65de32
rparkchan <rparkchan@college.harvard.edu>
2018-12-23 00:08:57 -0500

    cleaning up

--- playback.py
+++ playback.py
@@ -1,4 +1,4 @@
-#1A
+#1B make one or two
 from instrument import *
 
 # notelist = 'Db5 Ab4 B4 Db4 B3 Ab4 B4 Db4 B3 Ab4 B4'.split(' ') #notes or numbers
@@ -10,19 +10,20 @@ from instrument import *
 # notelist = 'Db5 Ab4 B4 Db4 B3 Ab4 B4 Db4 B3 Ab4 B4'.split(' ') #notes or numbers
 # lengthlist = [.72, .36, .36, .72, .72, .36, .36, .72, .72, .72, .72] #in seconds
 
-notelist = 'A B C D'.split(' ') #notes or numbers
-lengthlist = [.24] #in seconds
+notelist = 'B4 r D4 r'.split(' ') #notes or numbers
+lengthlist = [.96] #in seconds
 
-tremolofreq = [0] #in fq, repeating sequence
-vollist = [4] #value of 1 is appropriate, repeating sequence
+tremlen = 1000
+tremolofreq = [1/tremlen] #in fq, repeating sequence
+vollist = [3] #value of 1 is appropriate, repeating sequence
 panlist = [-1, 1] #value of 0 for mono, -1 for left, 1 for right, repeating sequence
 
 wavname = 'sounds/voice.wav'
 sqwave = True
 
-notelist = randnotes(notelist, conctimes = 40, lower = 3, upper = 5) #random generators
+# notelist = randnotes(notelist, conctimes = 40, lower = 3, upper = 5) #random generators
 # lengthlist = randnumlist(len(notelist), .06, .64)
-vollist = randnumlist(len(notelist), 1, 3)
+# vollist = randnumlist(len(notelist), 3,5)
 # tremolofreq = randnumlist(len(notelist), 1, 6)
 # panlist = randnumlist(len(notelist), -.5, .5)
 
