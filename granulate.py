0741c5cd9e5dd8812f36b479e85354b22dc57eea
rparkchan <rparkchan@college.harvard.edu>
2019-01-29 15:29:28 -0500

    moved constants

--- granulate.py
+++ granulate.py
@@ -6,6 +6,7 @@ from scipy.io import wavfile
 
 # constants
 VIB_PATCH = 4. # adjusts for length of fullsample
+PYAUDIO_CORR =4 # PyAudio, 4 for stereo, 2 for mono
 
 def tremolo(sample, freq):
 	x = np.linspace(3 * np.pi / 2, 7 * np.pi / 2, (44100 / freq))
@@ -19,7 +20,7 @@ def tremolo(sample, freq):
 def granulate(tone):
 	# fill a rest as 0s
 	if tone['note_freq'] == 0: 
-		fullsample = [0] * (int(tone['length'] * 44100))
+		fullsample = [0] * (int(tone['length'] * 44100 * PYAUDIO_CORR))
 		fullsample = np.array(fullsample).reshape(-1,1)
 		return np.concatenate((fullsample,fullsample), axis=1)
 	
@@ -44,12 +45,12 @@ def granulate(tone):
 	fullsample = []
 	smallsample = smallsample.tolist()
 	tnf = tone['note_freq']
-	while len(fullsample) < sr * tone['length']:
+	while len(fullsample) < sr * tone['length'] * PYAUDIO_CORR:
 		# vibrato (consider "ramping up," like the cool chiptune stuff or a violin)
 		# if len(fullsample) >= sr * tone['length'] / (2. * VIB_PATCH): # when to begin vib
 		if(tone['vib'] != 0): 
 			if i%int(tnf / tone['vib']) == 0: # speed (user-adjustable * fq-dependent)
-				if (j+1)%4 < 2: # depth (user-adjustable * fq-dependent)
+				if (j+2)%8 < 4: # depth (user-adjustable * fq-dependent)
 					smallsample += [[0,0]]
 				else:
 					del smallsample[-1]
@@ -60,7 +61,7 @@ def granulate(tone):
 		ind = -1 if random.random() > fractindex else len(smallsample)
 		fullsample += smallsample[:ind]
 		
-	fullsample = fullsample[0:int(sr * tone['length'])]
+	fullsample = fullsample[0:int(sr * tone['length'] * PYAUDIO_CORR)]
 	fullsample = np.array(fullsample, dtype = np.int16)
 
 	# tremolo
--- playback.py
+++ playback.py
@@ -1,5 +1,14 @@
 from instrument import *
 
+# input for tones
+notelist = '440 r E5 Db4'.split(' ') # notes or numbers
+lengthlist = [1.] # in seconds
+tremolofreq = [0, 0, 0, 0, 0] # in fq, repeating sequence
+viblist = [35] # 35 is appropriate
+vollist = [1000] # 1000 is appropriate, repeating sequence
+panlist = [0, 0, -1, 1] # 0 for mono, -1 for left, 1 for right, repeating sequence
+samplelist = ['sounds/voice.wav']
+
 # construct dictionary mapping i.e. "A4" to 440
 NOTES = ['C','Db','D','Eb','E','F','Gb','G','Ab','A','Bb','B']
 FREQS = [16.35, 17.32, 18.35, 19.45, 20.60, 21.83, 23.12, 24.50, 25.96, 27.50, 29.14, 30.87]
@@ -8,24 +17,12 @@ for i,note in enumerate(NOTES):
 	for j in range(20):
 		notetofreq[note + str(j)] = FREQS[i] * pow(2,j)
 
-# PyAudio constant, 4 for stereo, 2 for mono
-PYAUDIO_CORR = 4
-
-# input for tones
-notelist = '440 r E5 Db4'.split(' ') # notes or numbers
-lengthlist = [1.28] # in seconds
-tremolofreq = [10, 0, 4, 0, 0] # in fq, repeating sequence
-viblist = [0] # 25 is appropriate
-vollist = [1000] # 1000 is appropriate, repeating sequence
-panlist = [0, 0, -1, 1] # 0 for mono, -1 for left, 1 for right, repeating sequence
-samplelist = ['sounds/voice.wav']
-freqlist = [float(note) if note.isdigit() else float(notetofreq[note]) for note in notelist]
-
 # construct instrument
+freqlist = [float(note) if note.isdigit() else float(notetofreq[note]) for note in notelist]
 tonelist = []
 for i in range(len(notelist)):
   tonelist.append({'note_freq' : freqlist[i],
-                   'length' : lengthlist[i%len(lengthlist)] * PYAUDIO_CORR,
+                   'length' : lengthlist[i%len(lengthlist)],
                    'tremolo' : tremolofreq[i%len(tremolofreq)],
                    'vib' : viblist[i%len(viblist)],
                    'volume' : vollist[i%len(vollist)],
